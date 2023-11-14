f4132aaa0121e905860c45ead31a801051cdc6e6
Konrad Wulf <konrad.wulf@mesalvo.com>
2023-11-14 19:38:27 +0100

    #2 parse json returned from httpclient

--- src/test/java/solutions/wulf/httpclient/HttpClientTest.java
+++ src/test/java/solutions/wulf/httpclient/HttpClientTest.java
@@ -1,5 +1,7 @@
 package solutions.wulf.httpclient;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -24,5 +26,11 @@ class HttpClientTest {
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         assertTrue(response.body().contains("fault"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.body());
+        String msg = root.at("/fault/faultstring").asText();
+
+        assertTrue(msg != null && !msg.isEmpty());
     }
 }
