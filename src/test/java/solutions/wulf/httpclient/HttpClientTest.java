443dc81f11c3d61228808958cbbd58f59b0b6962
Konrad Wulf <konrad.wulf@mesalvo.com>
2023-11-14 22:56:16 +0100

    Closes #2 added async call back version

--- src/test/java/solutions/wulf/httpclient/HttpClientTest.java
+++ src/test/java/solutions/wulf/httpclient/HttpClientTest.java
@@ -1,9 +1,9 @@
 package solutions.wulf.httpclient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
@@ -11,13 +11,19 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class HttpClientTest {
+
+    private static String URI_EVENT_DISCOVERY = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=INVALID";
 
     @Test
     public void testGetErrorInvalidApiKey() throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=INVALID"))
+                .uri(new URI(URI_EVENT_DISCOVERY))
                 .GET()
                 .build();
 
@@ -25,12 +31,35 @@ class HttpClientTest {
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        assertTrue(response.body().contains("fault"));
+        assertFaultMessage(response.body());
+    }
+
+    @Test
+    public void testAsyncGetInvalidApiKey() throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URI_EVENT_DISCOVERY))
+                .timeout(Duration.ofSeconds(20))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+        HttpClient.newBuilder()
+                .build()
+                .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(HttpClientTest::assertFaultMessage);
+    }
+
+    private static void assertFaultMessage(String response)  {
+        assertTrue(response.contains("fault"));
 
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.body());
-        String msg = root.at("/fault/faultstring").asText();
-
-        assertTrue(msg != null && !msg.isEmpty());
+        try {
+            JsonNode root = mapper.readTree(response);
+            String msg = root.at("/fault/faultstring").asText();
+            assertTrue(msg != null && !msg.isEmpty());
+        } catch (JsonProcessingException e) {
+            fail(e.getMessage());
+        }
     }
 }
