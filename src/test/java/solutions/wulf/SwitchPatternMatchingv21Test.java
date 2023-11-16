79f4a71329be59ded3429bbdb85f849f1e081fca
Konrad Wulf <konrad.wulf@mesalvo.com>
2023-11-17 00:05:59 +0100

    #1 correcions to SwitchPatternMatchingv21Test

--- src/test/java/solutions/wulf/SwitchPatternMatchingv21Test.java
+++ src/test/java/solutions/wulf/SwitchPatternMatchingv21Test.java
@@ -1,25 +1,25 @@
 package solutions.wulf;
 
-import org.junit.jupiter.api.Test;
-import solutions.wulf.user.AdminUser;
-
 import static java.lang.System.out;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import solutions.wulf.user.AdminUser;
 
 public class SwitchPatternMatchingv21Test {
 
     @Test
     public void testMultiTypeFormatting() {
-        format(null);
-        format(32);
-        format(72985723L);
-        format(30.2d);
-        format("No Strings attached?");
-        format(new AdminUser()); // should trigger default
+    	List<Object> variousTypes = List.of(32, 72985723L, 30.2d, "No Strings attached?", new AdminUser());
+    	variousTypes.forEach(x -> out.println(format(x)));
+    	out.println(format(null));
     }
 
     static String format(Object obj) {
         return switch (obj) {
-            case null      -> out.println("nothing to format.");
+            case null      -> "nothing to format.";
             case Integer i -> String.format("int %d", i);
             case Long l    -> String.format("long %d", l);
             case Double d  -> String.format("double %f", d);
